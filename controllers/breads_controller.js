9021b997e0a831ac82de71b1f11e2fe0e8957688
Edwin Tolentino <edwintolentino@Edwins-Macbook.local>
2023-04-19 20:06:46 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,16 +1,39 @@
-const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
 
-module.exports = breads
-
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+breads.get("/:arrayIndex", (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.send("404");
+  }
+});
+
+// CREATE
+breads.post("/", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = "true";
+  } else {
+    req.body.hasGluten = "false";
+  }
+  Bread.push(req.body);
+  res.send(Bread);
+});
+
+
+module.exports = breads;
   
