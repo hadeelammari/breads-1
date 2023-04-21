1cf93a3ad96e11afb0498018629c3cd87b6709d6
Edwin Tolentino <edwintolentino@Edwins-Macbook.local>
2023-04-21 12:43:46 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,26 +14,27 @@ breads.get('/', (req, res) => {
 
 
 // SHOW
-breads.get("/:arrayIndex", (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render("Show", {
-      bread: Bread[req.params.arrayIndex],
-    });
-  } else {
-    res.send("404");
-  }
-});
+breads.get('/:arrayIndex', (req, res) => {
+    if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+      })
+    } else {
+      res.send('404')
+    }
+  })  
 
 // CREATE
-breads.post("/", (req, res) => {
-  if (req.body.hasGluten === "on") {
-    req.body.hasGluten = "true";
-  } else {
-    req.body.hasGluten = "false";
-  }
-  Bread.push(req.body);
-  res.send(Bread);
-});
+breads.post('/', (req, res) => {
+    if(req.body.hasGluten === 'on') {
+      req.body.hasGluten = 'true'
+    } else {
+      req.body.hasGluten = 'false'
+    }
+    Bread.push(req.body)
+    res.send(Bread)
+  })
+  
 
 
 module.exports = breads;
--- views/show.jsx
+++ views/show.jsx
@@ -6,23 +6,24 @@ function Show ({bread}) {
     // console.log(bread.name)
       return (
         <Default>
-        <h3>{bread.name}</h3>
-        <p>
-          and it
-          {
-            bread.hasGluten
-            ? <span> does </span>
-            : <span> does NOT </span>
-          }
-          have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
-      </Default>
-      
+  <h3>{bread.name}</h3>
+  <p>
+    and it
+    {
+      bread.hasGluten
+      ? <span> does </span>
+      : <span> does NOT </span>
+    }
+    have gluten.
+  </p>
+  <img src={bread.image} alt={bread.name} />
+  <li><a href="/breads">Go home</a></li>
+</Default>
+
       )
   }
   
 
 module.exports = Show
+
            
